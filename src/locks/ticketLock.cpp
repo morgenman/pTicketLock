4b2c503f00adb09e3a1a5ca7683ff5f8faef0ca6
morgendc203 <morgendc203@potsdam.edu>
2021-12-02 12:31:18 -0500

    Partially implemented TicketLock. stopping because I don't understand how threading is meant to work here.

--- src/locks/ticketLock.cpp
+++ src/locks/ticketLock.cpp
@@ -3,11 +3,27 @@
 #include <chrono>
 #include <thread>
 
-TicketLock::TicketLock(unsigned int turnCount) { maxTickets = turnCount; }
+#include "SpinLock.h"
 
-TicketLock::Ticket TicketLock::lock() volatile { return Ticket{0, 0}; }
+TicketLock::TicketLock(unsigned int turnCount) {
+  maxThreads = turnCount;
+  for (unsigned int i = 0; i < turnCount; i++) threads.push_back(i);
+  newTicketUnique = 0;
+  newTicketLock = {};
+  masterLock = {};
+}
 
-void TicketLock::unlock() volatile {}
+TicketLock::Ticket TicketLock::lock() volatile {
+  unsigned int ticket = getNext();
+  while (ticket != threads.at(ticket % maxThreads))
+    ;
+
+  return Ticket{ticket, threads};
+}
+
+void TicketLock::unlock() volatile {
+  threads = evil_increment(threads, 1, 0);
+}
 
 // slowly increment the given variable; increment and delay are both defaulted
 unsigned int TicketLock::evil_increment(
@@ -17,4 +33,13 @@ unsigned int TicketLock::evil_increment(
   std::this_thread::sleep_for(std::chrono::milliseconds(millisecondsToHold));
   toBeIncremented = initialValue + incrementToAdd;
   return toBeIncremented;
+}
+
+unsigned int TicketLock::getNext() volatile {
+  int out;
+  newTicketLock.lock();
+  out = newTicketUnique;
+  newTicketUnique = evil_increment(newTicketUnique);
+  newTicketLock.unlock();
+  return out;
 }
--- src/locks/ticketLock.h
+++ src/locks/ticketLock.h
@@ -1,26 +1,29 @@
 #ifndef TICKETLOCK_H
 #define TICKETLOCK_H
 #include <string>
+#include <vector>
+
 #include "SpinLock.h"
 /**
  * Implementation of the ticket lock type. Note that rather than a
- * queue, the ticket and turn can be represented an integers that
+ * queue, the ticket and turn can be represented as integers that
  * count modulo some maximum number. To be safe, making that number
  * greater than or equal to the number of threads is best.
  */
 class TicketLock {
-public:
+ public:
   /**
    * Return type for call to lock. Includes the ticket value, ticket,
    * and the actual turn value the lock had when the ticket was
    * returned.
    */
   class Ticket {
-  public:
+   public:
     const unsigned int ticket;
     const unsigned int initial_turn;
-    Ticket(unsigned int t, unsigned int i) :
-      ticket{t}, initial_turn{i} { /*Empty body */}
+    Ticket(unsigned int t, unsigned int i)
+        : ticket{t}, initial_turn{i} { /*Empty body */
+    }
   };
 
   /**
@@ -53,27 +56,35 @@ public:
    */
   void unlock() volatile;
 
-private:
+ private:
   /**
-   * The increment function for any member fields of this class. Do not use ++x or x++.
-   * This function takes a little time (based on the last parameter) to perform the
-   * increment, holding the old value of the variable locally. This increases the chances
-   * of a race condition.
+   * The increment function for any member fields of this class. Do not use ++x
+   * or x++. This function takes a little time (based on the last parameter) to
+   * perform the increment, holding the old value of the variable locally. This
+   * increases the chances of a race condition.
    *
-   * @param toBeIncremented the lvalue that is actually changed by the end of this method
+   * @param toBeIncremented the lvalue that is actually changed by the end of
+   * this method
    * @param incrementToAdd amount by which to increment the variable; default: 1
    * @param millisecondsToHold delay between read and update; default: 25
    */
-  unsigned int  evil_increment(volatile unsigned int &toBeIncremented,
-                               int incrementToAdd = 1,
-                               unsigned int milliseconds = 25) volatile;
+  unsigned int evil_increment(volatile unsigned int &toBeIncremented,
+                              int incrementToAdd = 1,
+                              unsigned int milliseconds = 25) volatile;
 
   // Implementation details. May include fields (turn, ticket, ...) and methods.
   // Might make use of spin locks, xchg (since it is in the source code), and,
   // perhaps, methods from the book/slides pertaining to a ticket lock.
+  unsigned int newTicketUnique;
+  vector<unsigned int> threads;
+  unsigned int maxThreads;
+  SpinLock newTicketLock;
+  SpinLock masterLock;
 
-  int ticket;
-  int currentTurn;
-  int maxTickets;
+  /**
+   *
+   * @return {unsigned}  :
+   */
+  unsigned int getNext() volatile;
 };
 #endif /* TICKETLOCK_H */
